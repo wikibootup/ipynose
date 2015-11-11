716a8cf56597c5c79b64c98b87a607ac45fed2ec
wikibootup <wikibootup@gmail.com>
2015-11-11 19:01:45 +0900

    change test variable name

--- tests/test_ipynose.py
+++ tests/test_ipynose.py
@@ -3,8 +3,8 @@ from .. import ipynose
 
 class TestIpynose(unittest.TestCase):
     def setUp(self):
-        self.TRUETESTPATH = "tests/test_true.py"
-        self.FALSETESTPATH = "tests/test_false.py"
+        self.TRUE_TEST_PATH = "tests/test_true.py"
+        self.FALSE_TEST_PATH = "tests/test_false.py"
 
     def test_all_nose_results(self):
         """
@@ -13,7 +13,7 @@ class TestIpynose(unittest.TestCase):
         pass
 
     def test_nose_result_with_true_test(self):
-        self.assertTrue("OK" in ipynose.nose_result(self.TRUETESTPATH))
+        self.assertTrue("OK" in ipynose.nose_result(self.TRUE_TEST_PATH))
 
     def test_nose_result_with_false_test(self):
-        self.assertTrue("FAILED" in ipynose.nose_result(self.FALSETESTPATH))
+        self.assertTrue("FAILED" in ipynose.nose_result(self.FALSE_TEST_PATH))
