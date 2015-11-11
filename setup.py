84da392941ac3acfecbd3d8b17f4f7e521d93cdf
wikibootup <wikibootup@gmail.com>
2015-11-11 22:19:31 +0900

    basic package configuration

---
+++ setup.py
@@ -0,0 +1,41 @@
+"""
+Reference:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+from setuptools import setup
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ipynose',
+    version='0.0.1',
+    description='print nosetests result in the iPython notebook',
+    long_description=long_description,
+    url='https://github.com/wikibootup/ipynose',
+    author='wikibootup',
+    author_email='.',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='nose',
+    install_requires=['nose'],
+)
+
